98b320ed326d1a45b8371e67f902fa912872f5e1
github username <github email address>
2018-08-03 11:24:12 -0500

    module challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,23 @@ class Movie
     @@all
   end
 
+
+  def queue_items
+    QueueItem.all.select{|item| item.movies == self}
+
 end
+
+   def viewers
+     queue_items.map{|viewer| viewer.movies}
+   end
+
+
+     def average_rating
+       queue_items = QueueItem.all.select { |QueueItem| QueueItem.movie == self }
+       total_ratings = queue_items.reduce(0) {|sum, list|
+         list.rating ? sum + list.rating : nil
+       }
+       total_ratings ? total_ratings/queue_items.length : "No ratings for this film."
+     end
+
+   end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -6,10 +6,30 @@ class Viewer
   def initialize(username)
     @username = username
     self.class.all << self
+    @viewrsqueue = []
+
   end
 
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select{|item| item.viewer == self }
 end
+  def queue_movies
+    queue_items.map{|item| item.movie}
+end
+
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
+end
+
+
+  def rate_movie(movie, rating)
+    movie_watched = queue_items.find { |w| w.movie == movie }
+    movie_watched.rating = rating
+  end
+
+end 
