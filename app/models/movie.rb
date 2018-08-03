099c1ab136ac1c6f162ac5c1fab1d672bb2c13cb
yendai-vo <yendai.vo@hotmail.com>
2018-08-03 11:19:20 -0500

    finished most methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,26 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select do |item|
+      item.movie == self
+    end
+  end
+
+  def viewers
+    queue_items.map(&:viewer)
+  end
+
+  def average_rating
+    count = queue_items.length
+    sum = queue_items.map(&:rating).reduce(:+)
+    sum / count
+  end
+
+  # def self.highest_rated
+  #   queue= QueueItem.self.all.select do |item|
+  #     item.movie == self
+  #   end
+  #   queue.map(&:rating).max
+  # end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+cindy = Viewer.new("cindy_v")
+daniel = Viewer.new("daniel_s")
+carlos = Viewer.new("carlos_s")
+
+movie1 = Movie.new("King Kong")
+movie2 = Movie.new("Harry Potter")
+movie3 = Movie.new("Star Wars")
+
+first = QueueItem.new(cindy, movie1, 4)
+second = QueueItem.new(daniel, movie2, 4)
+third = QueueItem.new(cindy, movie3, 5)
+fourth = QueueItem.new(carlos, movie1, 2)
+
+
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+ #leave this here to ensure binding.pry isn't the last line
+puts "Hello World!"
