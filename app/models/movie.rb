f12129e8718da80fbe1914c185809cf83e0cfde5
Kelly <devkells00@gmail.com>
2018-08-03 11:18:35 -0500

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,35 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select do |qitem|
+      qitem.movie == self
+    end
+  end
+
+  def queue_viewers
+    my_viewers = []
+    self.queue_items.map do |qviewers|
+      my_viewers << qviewers.viewer
+    end
+    my_viewers
+  end
+
+  def average_rating
+    ratings = []
+    self.queue_items.map do |qitem|
+      ratings << qitem.rating
+    end
+    (ratings.reduce(:+))/(ratings.length)
+  end
+
+  def self.highest_rated
+    avg_rating = []
+    self.all.each do |movie|
+    avg_rating << movie.average_rating
+    end
+    avg_rating.max
+    #returning highest average rating, need movie name
+  end
+
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,27 @@ class Viewer
   def self.all
     @@all
   end
+
+  def queue_items
+    QueueItem.all.select do |qitem|
+      qitem.viewer == self
+    end
+  end
   
+  def queue_movies
+    my_movies = []
+    self.queue_items.map do |qmovies|
+      my_movies << qmovies.movie
+    end
+    my_movies
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(movie, self)
+  end
+
+  def rate_movie(movie, rating)
+    
+  end
+
 end
