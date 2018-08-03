43eb55d255031d09a49d2f1b87104fe03c395644
github username <github email address>
2018-08-03 11:20:45 -0500

    -

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,34 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select do |queueitem|
+      queueitem.movie == self
+    end
+  end
+
+  def viewers
+    queue_items.map do |queue_item|
+      queue_item.viewer
+    end
+  end
+
+  def average_rating
+    total_rating = queue_items.reduce(0.00) do |total, current_queueitem|
+      total += current_queueitem.rating
+  end
+    total_rating/queue_items.length
+end
+
+def self.highest_rated
+  movie_avg_ratings = self.all.collect do |movie|
+    movie.average_rating
+  end
+  movie_avg_ratings.max
+  # movie_avg_ratings.reduce do |movie_instances_total, movie_instance_current|
+  #   if movie_instances_total > movie_instance_current
+  #     binding.pry
+  #     puts movie_instances_total
+    end
+
 end
